9e6985e3e6551149e1ac4a090c5abd4ebcd56c72
Alec Skinner <skinner452@gmail.com>
2024-11-26 11:40:12 -0500

    Close menu when navigating to home

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -22,7 +22,7 @@ const Navbar = () => {
   return (
     <nav className="bg-slate-900 px-10">
       <div className="mx-auto max-w-5xl flex flex-row items-center justify-between">
-        <Link to="/" className="font-semibold py-5">
+        <Link to="/" className="font-semibold py-5" onClick={closeMenu}>
           Alec Skinner
         </Link>
 
